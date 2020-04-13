b852d0c21b6ad62093ed8bdc8299816587b69415
Sergii Kostyrko <srg.kostyrko@gmail.com>
2020-04-13 22:27:10 +0300

    feat: build backward compatible qsp version

--- libs/qsp-wasm/src/wasm/qsp.js
+++ libs/qsp-wasm/src/wasm/qsp.js
@@ -622,8 +622,8 @@ var Module = (function () {
       Module['HEAPF32'] = HEAPF32 = new Float32Array(buf);
       Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
     }
-    var DYNAMIC_BASE = 5760928,
-      DYNAMICTOP_PTR = 517888;
+    var DYNAMIC_BASE = 5761040,
+      DYNAMICTOP_PTR = 518e3;
     var INITIAL_INITIAL_MEMORY = Module['INITIAL_MEMORY'] || 33554432;
     if (Module['wasmMemory']) {
       wasmMemory = Module['wasmMemory'];
