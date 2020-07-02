bc3c5d5b5c0ce788fea1a2054011b92129e04be2
Sergii Kostyrko <srg.kostyrko@gmail.com>
2020-07-02 07:13:27 +0300

    refactor(player): rename new player folder

--- apps/player/jest.config.js
+++ apps/player/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'qplayer',
+  name: 'player',
   preset: '../../jest.config.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/qplayer',
+  coverageDirectory: '../../coverage/apps/player',
 };
