07416847ce95c0d0cc57dc09d0bd408222767bfe
Sergii Kostyrko <srg.kostyrko@gmail.com>
2020-07-02 07:07:31 +0300

    refactor(player): remove old player version

--- apps/player/src/app/game.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { observer } from 'mobx-react-lite';
-
-import { useGameManager } from './game/manager';
-
-export const Game: React.FC = observer(({ children }) => {
-  const manager = useGameManager();
-  return manager.isInitialized ? <>{children}</> : <div>Loading</div>;
-});
