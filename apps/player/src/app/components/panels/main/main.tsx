544667f353a5f188ca8e704ecb8af5884240212e
Sergii Kostyrko <srg.kostyrko@gmail.com>
2020-09-07 22:40:24 +0300

    feat: autoscroll panel if content was appended

--- apps/player/src/app/components/panels/main/main.tsx
+++ apps/player/src/app/components/panels/main/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import CustomScroll from 'react-custom-scroll';
+import { usePrevious } from 'react-delta';
 import { PanelWithBackground, PanelContent } from '../../ui-blocks/panel';
 import { useGameManager } from '../../../game/manager';
 import { Content } from '../../content/content';
 
 export const MainPanel: React.FC = observer(() => {
   const manager = useGameManager();
+  const prevMain = usePrevious(manager.main);
+  const scrollTo = prevMain && manager.main !== prevMain && manager.main.startsWith(prevMain) ? 10000 : undefined;
   return (
     <PanelWithBackground>
-      <CustomScroll heightRelativeToParent="100%">
+      <CustomScroll heightRelativeToParent="100%" scrollTo={scrollTo}>
         <PanelContent>
           <Content content={manager.main} />
         </PanelContent>
