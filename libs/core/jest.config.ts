8b5a280c75562fe02a5b2880a90aed3d474a0892
Sergii Kostyrko <srg.kostyrko@gmail.com>
2022-09-12 15:39:01 +0300

    chore: update nx

--- libs/core/jest.config.ts
+++ libs/core/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'core',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/core',
