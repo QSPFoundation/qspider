f677ed606dbdc4a950d37de7197579698e6c20a9
Sergii Kostyrko <srg.kostyrko@gmail.com>
2022-10-04 15:06:26 +0300

    chore: fix updater links generation

--- tools/autoupdater-version-updater.js
+++ tools/autoupdater-version-updater.js
@@ -8,8 +8,8 @@ module.exports.writeVersion = function (contents, version) {
   const json = JSON.parse(contents);
   json.name = version;
   json.pub_date = new Date().toISOString();
-  json.platforms.darwin.url = `https://github.com/QSPFoundation/qspider/releases/download/${version}/qSpider.app.tar.gz`;
-  json.platforms.linux.url = `https://github.com/QSPFoundation/qspider/releases/download/${version}/qSpider.AppImage.tar.gz`;
-  json.platforms.win64.url = `https://github.com/QSPFoundation/qspider/releases/download/${version}/qSpider.x64.msi.zip`;
+  json.platforms.darwin.url = `https://github.com/QSPFoundation/qspider/releases/download/v${version}/qSpider.app.tar.gz`;
+  json.platforms.linux.url = `https://github.com/QSPFoundation/qspider/releases/download/v${version}/qSpider.AppImage.tar.gz`;
+  json.platforms.win64.url = `https://github.com/QSPFoundation/qspider/releases/download/v${version}/qSpider.x64.msi.zip`;
   return stringifyPackage(json, 2, '\r\n');
 };
