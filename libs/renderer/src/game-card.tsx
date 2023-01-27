18a9d85cc4913d051fc819f4f92fec144011d08b
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-01-27 15:35:00 +0200

    feat: show game description in popover

--- libs/renderer/src/game-card.tsx
+++ libs/renderer/src/game-card.tsx
@@ -1,6 +1,7 @@
 import { GameDescriptor } from '@qspider/contracts';
 import { games$ } from '@qspider/game-state';
 import { Cross1Icon } from '@radix-ui/react-icons';
+import * as Popover from '@radix-ui/react-popover';
 import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { use } from 'xoid';
@@ -11,26 +12,41 @@ export const GameCard: React.FC<{ game: GameDescriptor }> = ({ game }) => {
     use(games$).remove(game.id);
   }, [game.id]);
   return (
-    <div className="game-shelf__card">
-      <div className="game-shelf__card-content">
-        <button
-          type="button"
-          className="q-ghost-button q-remove-game-button"
-          title="Remove from Game Shelf"
-          onClick={removeGame}
-        >
-          <Cross1Icon />
-        </button>
-        <h3 className="game-shelf__card-title">{game.title}</h3>
+    <Popover.Root>
+      <div className="game-shelf__card">
+        <div className="game-shelf__card-content">
+          <button
+            type="button"
+            className="q-ghost-button q-remove-game-button"
+            title="Remove from Game Shelf"
+            onClick={removeGame}
+          >
+            <Cross1Icon />
+          </button>
+          <h3 className="game-shelf__card-title">{game.title}</h3>
+        </div>
+        <div className="game-shelf__card-actions">
+          {game.description ? (
+            <Popover.Trigger asChild>
+              <button className="q-ghost-button">Read Description</button>
+            </Popover.Trigger>
+          ) : (
+            <div></div>
+          )}
+          <Link to={`/game/${game.id}`}>Run</Link>
+        </div>
         {game.description && (
-          <div>
-            <ContentRenderer content={game.description} />
-          </div>
+          <Popover.Portal>
+            <Popover.Content className="q-popover-content" sideOffset={5}>
+              <ContentRenderer content={game.description} />
+              <Popover.Close className="q-ghost-button q-popover-close" aria-label="Close">
+                <Cross1Icon />
+              </Popover.Close>
+              <Popover.Arrow className="q-popover-arrow" />
+            </Popover.Content>
+          </Popover.Portal>
         )}
       </div>
-      <div className="game-shelf__card-actions">
-        <Link to={`/game/${game.id}`}>Run</Link>
-      </div>
-    </div>
+    </Popover.Root>
   );
 };
--- libs/renderer/src/qsp-catalog-game-card.tsx
+++ libs/renderer/src/qsp-catalog-game-card.tsx
@@ -1,55 +1,52 @@
 import { CatalogGame, moveToShelf } from '@qspider/game-state';
-import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import { Cross1Icon } from '@radix-ui/react-icons';
+import * as Popover from '@radix-ui/react-popover';
 import { DateTime } from 'luxon';
-import { useState } from 'react';
 import { ContentRenderer } from './content-renderer';
 import { formatBytes } from './formatters';
 
 export const CatalogGameCard: React.FC<{ game: CatalogGame }> = ({ game }) => {
-  const [isDescriptionShown, setIsDescriptionShown] = useState(false);
   const icon = game.icon ? game.icon.substring(game.icon.lastIndexOf('com_sobi2')) : null;
   return (
-    <div className="q-catalog__card">
-      <h5 className="q-title">
-        {game.icon && <img alt="" src={'https://qsp.su/gamestock/image.php?name=' + icon} loading="lazy" />}
-        {game.title}
-      </h5>
+    <Popover.Root>
+      <div className="q-catalog__card">
+        <h5 className="q-title">
+          {game.icon && <img alt="" src={'https://qsp.su/gamestock/image.php?name=' + icon} loading="lazy" />}
+          {game.title}
+        </h5>
 
-      <div className="q-catalog__card-details">
-        <div>
-          <div className="q-catalog__card-details-row">Author: {game.author}</div>
-          {game.ported_by && <div className="q-catalog__card-details-row">Ported by: {game.ported_by}</div>}
-          <div className="q-catalog__card-details-row">Version: {game.version}</div>
-        </div>
-        <div>
-          <div className="q-catalog__card-details-row">Size: {formatBytes(game.file_size)}</div>
-          <div className="q-catalog__card-details-row">Type: {game.file_ext}</div>
-          <div className="q-catalog__card-details-row">
-            Last update: {DateTime.fromMillis(game.mod_date).toLocaleString(DateTime.DATETIME_FULL)}
+        <div className="q-catalog__card-details">
+          <div>
+            <div className="q-catalog__card-details-row">Author: {game.author}</div>
+            {game.ported_by && <div className="q-catalog__card-details-row">Ported by: {game.ported_by}</div>}
+            <div className="q-catalog__card-details-row">Version: {game.version}</div>
+          </div>
+          <div>
+            <div className="q-catalog__card-details-row">Size: {formatBytes(game.file_size)}</div>
+            <div className="q-catalog__card-details-row">Type: {game.file_ext}</div>
+            <div className="q-catalog__card-details-row">
+              Last update: {DateTime.fromMillis(game.mod_date).toLocaleString(DateTime.DATETIME_FULL)}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="q-catalog__card-buttons">
-        <button className="q-ghost-button" onClick={(): void => setIsDescriptionShown(!isDescriptionShown)}>
-          {isDescriptionShown ? (
-            <>
-              Hide Description <ChevronUpIcon />
-            </>
-          ) : (
-            <>
-              Show Description <ChevronDownIcon />
-            </>
-          )}
-        </button>
-        <button className="q-button" onClick={(): Promise<void> => moveToShelf(game)}>
-          Add to shelf
-        </button>
-      </div>
-      {isDescriptionShown && (
-        <div className="q-catalog__card-description">
-          <ContentRenderer content={game.description} />
+        <div className="q-catalog__card-buttons">
+          <Popover.Trigger asChild>
+            <button className="q-ghost-button">Read Description</button>
+          </Popover.Trigger>
+          <button className="q-button" onClick={(): Promise<void> => moveToShelf(game)}>
+            Add to shelf
+          </button>
         </div>
-      )}
-    </div>
+        <Popover.Portal>
+          <Popover.Content className="q-popover-content" sideOffset={5}>
+            <ContentRenderer content={game.description} />
+            <Popover.Close className="q-ghost-button q-popover-close" aria-label="Close">
+              <Cross1Icon />
+            </Popover.Close>
+            <Popover.Arrow className="q-popover-arrow" />
+          </Popover.Content>
+        </Popover.Portal>
+      </div>
+    </Popover.Root>
   );
 };
