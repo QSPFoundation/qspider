856188ebc381b28c754ab6b2057517540efbc34b
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-03-21 16:57:51 +0200

    chore: e2e infa improvements

--- apps/player-e2e/src/integration/app.cy.ts
+++ apps/player-e2e/src/integration/app.cy.ts
@@ -1,11 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
 describe('player', () => {
-  beforeEach(() => {
-    cy.setup_intercepts();
-    cy.visit('/?config=test-assets/game.cfg');
-  });
-
   it('should display welcome message', () => {
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('spider');
--- apps/player-e2e/src/support/commands.ts
+++ apps/player-e2e/src/support/commands.ts
@@ -7,11 +7,7 @@ declare namespace Cypress {
 }
 
 Cypress.Commands.add('setup_intercepts', () => {
-  cy.intercept('test-assets/game.cfg', { fixture: 'test-assets/game.cfg' });
-  cy.intercept('test-assets/aero/game.qsps', { fixture: 'test-assets/aero/game.qsps' });
-  cy.intercept('test-assets/classic/game.qsps', { fixture: 'test-assets/classic/game.qsps' });
-  cy.intercept('test-assets/common/game.qsps', { fixture: 'test-assets/common/game.qsps' });
-  cy.intercept('test-assets/qspider/game.qsps', { fixture: 'test-assets/qspider/game.qsps' });
+  //
 });
 //
 // -- This is a child command --
