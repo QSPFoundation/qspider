36628418072519adbe787d93142c28c4b0e8fc75
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-03-26 19:33:43 +0300

    fix: fix rendering of svg tag with capital letter in name

--- libs/renderer/src/content/element.tsx
+++ libs/renderer/src/content/element.tsx
@@ -28,8 +28,48 @@ const voidTags = [
   'wbr',
 ];
 
+export const TAGS_MAP: Record<string, string> = {
+  altglyph: 'altGlyph',
+  altglyphdef: 'altGlyphDef',
+  altglyphitem: 'altGlyphItem',
+  animatecolor: 'animateColor',
+  animatemotion: 'animateMotion',
+  animatetransform: 'animateTransform',
+  clippath: 'clipPath',
+  feblend: 'feBlend',
+  fecolormatrix: 'feColorMatrix',
+  fecomponenttransfer: 'feComponentTransfer',
+  fecomposite: 'feComposite',
+  feconvolvematrix: 'feConvolveMatrix',
+  fediffuselighting: 'feDiffuseLighting',
+  fedisplacementmap: 'feDisplacementMap',
+  fedistantlight: 'feDistantLight',
+  fedropshadow: 'feDropShadow',
+  feflood: 'feFlood',
+  fefunca: 'feFuncA',
+  fefuncb: 'feFuncB',
+  fefuncg: 'feFuncG',
+  fefuncr: 'feFuncR',
+  fegaussianblur: 'feGaussianBlur',
+  feimage: 'feImage',
+  femerge: 'feMerge',
+  femergenode: 'feMergeNode',
+  femorphology: 'feMorphology',
+  feoffset: 'feOffset',
+  fepointlight: 'fePointLight',
+  fespecularlighting: 'feSpecularLighting',
+  fespotlight: 'feSpotLight',
+  fetile: 'feTile',
+  feturbulence: 'feTurbulence',
+  foreignobject: 'foreignObject',
+  glyphref: 'glyphRef',
+  lineargradient: 'linearGradient',
+  radialgradient: 'radialGradient',
+};
+
 export const Element: React.FC<ElementProps> = ({ attrs = {}, children = null, tagName }) => {
-  const [Tag, style, attributes] = useAttributes(attrs, tagName as keyof JSX.IntrinsicElements);
+  const tag = tagName in TAGS_MAP ? TAGS_MAP[tagName] : tagName;
+  const [Tag, style, attributes] = useAttributes(attrs, tag as keyof JSX.IntrinsicElements);
   const selfClose = voidTags.includes(tagName);
   return selfClose ? (
     <Tag style={style} {...attributes} />
