1ac4eda3e8d40120f24cc3eb25527676c5a917a8
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-11-12 12:14:14 +0200

    fix: fix view display in aero, use react-spring for animating view

--- libs/renderer/src/transformers/aero/aero-view.tsx
+++ libs/renderer/src/transformers/aero/aero-view.tsx
@@ -1,7 +1,9 @@
 import { Attributes, useQspVariable, viewPath$ } from '@qspider/game-state';
 import { useAtom } from '@xoid/react';
 import { ReactNode } from 'react';
+import { animated } from '@react-spring/web';
 import { useAttributes } from '../../content/attributes';
+import { useAeroEffect } from './use-aero-effect';
 
 export const AeroQspView: React.FC<{ attrs: Attributes; modal?: boolean; children: ReactNode }> = ({
   attrs,
@@ -10,7 +12,7 @@ export const AeroQspView: React.FC<{ attrs: Attributes; modal?: boolean; childre
   const [Tag, style, attributes] = useAttributes(attrs, 'qsp-view');
   const path = useAtom(viewPath$);
   const alwaysShow = useQspVariable('ALWAYS_SHOW_VIEW', '', 0, 0);
-  if (!path) return null;
+  const transitions = useAeroEffect(Boolean(path), '$VIEW_EFFECT', 'VIEW_EFFECT_TIME');
   const preparedStyle = {
     ...style,
     '--view-image': `url("${path}")`,
@@ -20,9 +22,13 @@ export const AeroQspView: React.FC<{ attrs: Attributes; modal?: boolean; childre
       viewPath$.set('');
     }
   };
-  return (
-    <Tag {...attributes} style={preparedStyle} onClick={onClick}>
-      {children}
-    </Tag>
+  return transitions((styles, open) =>
+    open ? (
+      <animated.div style={styles}>
+        <Tag {...attributes} style={preparedStyle} onClick={onClick}>
+          {children}
+        </Tag>
+      </animated.div>
+    ) : null
   );
 };
