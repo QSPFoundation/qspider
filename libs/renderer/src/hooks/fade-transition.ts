998130eba1d577d56c26ec6b75e6dff552ebf57a
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-12-27 16:10:24 +0200

    chore: add styles to qspider test asset

--- libs/renderer/src/hooks/fade-transition.ts
+++ libs/renderer/src/hooks/fade-transition.ts
@@ -2,8 +2,8 @@ import { TransitionFn, useTransition } from '@react-spring/web';
 
 export function useFadeTransition(state: boolean): TransitionFn {
   return useTransition(state, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
+    from: { opacity: 0, transform: 'translate(0, -2%) scale(0.96)' },
+    enter: { opacity: 1, transform: 'translate(0, 0) scale(1)' },
+    leave: { opacity: 0, transform: 'translate(0, -2%) scale(0.96)' },
   }) as unknown as TransitionFn;
 }
