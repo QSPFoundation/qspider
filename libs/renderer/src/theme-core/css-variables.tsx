115579400df6357a677c0fb8ff16512631c3376e
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-01-06 11:39:17 +0200

    fix: fix wrong image size in aero mode

--- libs/renderer/src/theme-core/css-variables.tsx
+++ libs/renderer/src/theme-core/css-variables.tsx
@@ -1,7 +1,7 @@
 import { CssVarDefinition, baseUrl$, currentCssVariables$, initialBaseUrl$, useQspVariable } from '@qspider/game-state';
 import { convertColor, getContrastColor, invertColor } from '@qspider/utils';
-import { useAtom } from '@xoid/react';
-import { useImageSize } from '../hooks/image-size';
+import { useAtom, useSetup } from '@xoid/react';
+import create from 'xoid';
 
 export const QspCSSVariables: React.FC = () => {
   const variables = useAtom(currentCssVariables$);
@@ -13,23 +13,54 @@ export const QspCSSVariables: React.FC = () => {
     </>
   );
 };
+interface ImageSize {
+  width: number;
+  height: number;
+}
 
-const QspCssVariableResource: React.FC<{ name: string; url: string; withSize: boolean }> = ({
-  name,
-  url,
-  withSize,
-}) => {
+const imageSizes$ = create<{ [key: string]: ImageSize }>({});
+
+const QspCssVariableResource: React.FC<{ name: string; url: string; withSize: boolean }> = (props) => {
+  const size$ = useSetup((props$) => {
+    const url$ = props$.focus((s) => s.url);
+    url$.watch((url) => {
+      url = prepareUrl(url);
+      if (!url) return;
+      if (!imageSizes$.value[url]) {
+        const img = document.createElement('img');
+        img.onload = (): void => {
+          const size = {
+            width: img.naturalWidth,
+            height: img.naturalHeight,
+          };
+          imageSizes$.update((s) => ({ ...s, [url]: size }));
+        };
+        img.src = url;
+      }
+    });
+    return create((get) => {
+      const url = prepareUrl(get(url$));
+      return get(imageSizes$)[url] ?? { width: 0, height: 0 };
+    });
+  }, props);
+  const size = useAtom(size$);
+  let { url } = props;
+  url = prepareUrl(url);
+  const { name } = props;
+
+  const sizeDefinitions = props.withSize ? `${name}-w: ${size.width}px; ${name}-h: ${size.height}px` : '';
+  const content = `qsp-game-root, #portal-container {${name}: ${url ? `url("${url}")` : 'none'};${sizeDefinitions}}`;
+  return <style>{content}</style>;
+};
+
+function prepareUrl(url: string): string {
   if (url.startsWith('qspider:')) {
     url = url.replace('qspider:', initialBaseUrl$.value);
   } else {
     url = baseUrl$.value + url;
   }
-  url = url.replace('\\', '/');
-  const size = useImageSize(url);
-  const sizeDefinitions = withSize ? `${name}-w: ${size.width}px; ${name}-h: ${size.height}px` : '';
-  const content = `qsp-game-root, #portal-container {${name}: ${url ? `url("${url}")` : 'none'};${sizeDefinitions}}`;
-  return <style>{content}</style>;
-};
+  return url.replace('\\', '/');
+}
 
 export const QspCssVariable: React.FC<{ definition: CssVarDefinition }> = ({ definition }) => {
   const value = useQspVariable(definition.from, '', 0, '');
