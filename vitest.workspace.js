9a549dee9b1fdd5ec92af9de31b252c89a195f96
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-07-15 17:36:50 +0300

    chore: delete cl-builder

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -4,5 +4,4 @@ export default defineWorkspace([
   './apps/player/vite.config.mts',
   './apps/player-desktop/vite.config.mts',
   './apps/player-standalone/vite.config.mts',
-  './apps/cl-builder/vite.config.mts',
 ]);
