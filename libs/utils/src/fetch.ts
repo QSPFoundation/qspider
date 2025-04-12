30f9f8d8bb9a2f9000775a64b5afc0dc6caf0641
Sergey Kostyrko <srg.kostyrko@gmail.com>
2025-04-12 08:45:19 +0300

    fix: fix loading games from url containing non asci characters

--- libs/utils/src/fetch.ts
+++ libs/utils/src/fetch.ts
@@ -1,5 +1,5 @@
 const PROXY_URL = 'https://proxy.qspider.xyz/';
 
 export function fetchProxyFallback(url: string): Promise<Response> {
-  return fetch(url).catch(() => fetch(PROXY_URL + window.btoa(url)));
+  return fetch(url).catch(() => fetch(PROXY_URL + window.btoa(encodeURI(url))));
 }
